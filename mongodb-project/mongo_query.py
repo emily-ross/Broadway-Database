e9712d582bbc65bd53615f45a3363712cc3f5c9d
Emily Ross <cellogirl76@gmail.com>
2015-06-10 14:24:58 -0700

    fully functioning (not pretty)
    these edits now make the code work as a whole! it’s not pretty, like
    the prints of the documents aren’t formatted quite yet, but it works.

--- mongodb-project/mongo_query.py
+++ mongodb-project/mongo_query.py
@@ -2,19 +2,15 @@ import pymongo
 import pprint
 
 from pymongo import MongoClient
+from flask import request
 
 client = MongoClient()
 db = client.Broadway
 collection = db.musicals
 
+def query(request):
+	category = request.form["category"]
+	term = request.form["search"]
 
-while True:
-	print "What category would you like to search: a title, main_character, playwright, instrument, opening, voca_part, vocal_range, age, or gender?"
-	category = raw_input()
-	print "Enter your", category, "here."
-	term = raw_input()
-
-	pp = pprint.PrettyPrinter(indent = 4)
-	for result in collection.find({category: term}):
-	pp.pprint(result)
-	
+	result = collection.find({category: term})
+	return result
--- server/__init__.py
+++ server/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template, request
 import sys
 sys.path.append("/Users/emiross/Desktop/Compsci/Mongo Project/mongodb-project/")
 import mongo_ask_insert
+import mongo_query
 
 app = Flask(__name__, template_folder='../website')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def serve_app():
-	return render_template('mongo_query.html')
+	result = None
+	if request.method == 'POST':
+			result = mongo_query.query(request)
+	return render_template('mongo_query.html', result=result)
 
 @app.route('/input', methods=['GET', 'POST'])
 def serve_input():
